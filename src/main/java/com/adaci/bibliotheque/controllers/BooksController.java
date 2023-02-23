2ebe07cb765bc95b6439643f617f564e11345841
AyouLasme <ayou.lasme@gmail.com>
2023-02-23 11:50:26 +0100

    Point d'arret validation

--- src/main/java/com/adaci/bibliotheque/controllers/BooksController.java
+++ src/main/java/com/adaci/bibliotheque/controllers/BooksController.java
@@ -5,13 +5,16 @@ import com.adaci.bibliotheque.models.Librairies;
 import com.adaci.bibliotheque.repository.BooksRepository;
 import com.adaci.bibliotheque.repository.LibrairiesRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.validation.FieldError;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.*;
 
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 import java.util.Optional;
 
 
@@ -36,5 +39,15 @@ public class BooksController {
     } else {
       return ResponseEntity.notFound().build();
     }
+  }
+
+  @PostMapping("/save")
+  public Books save(@RequestBody Books books) {
+    return booksRepository.save(books);
+  }
+
+  @PutMapping("/update")
+  public ResponseEntity<Books> update(@Validated @RequestBody Books books){
+    return null;
   }
 }
--- src/main/java/com/adaci/bibliotheque/models/Books.java
+++ src/main/java/com/adaci/bibliotheque/models/Books.java
@@ -1,9 +1,13 @@
 package com.adaci.bibliotheque.models;
 
 import jakarta.persistence.*;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
 import lombok.Getter;
 import lombok.Setter;
 
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
 import java.io.Serializable;
 import java.util.Date;
 
@@ -14,15 +18,23 @@ import java.util.Date;
 @Table(name = "books")
 public class Books implements Serializable {
   @Id
-  @GeneratedValue(strategy = GenerationType.AUTO)
+  @GeneratedValue(strategy = GenerationType.IDENTITY)
   private long id;
+  @NotNull(message = "Le champ name est obligatoire")
+  @NotBlank(message = "Le champ name ne peut etre vide")
   private String name;
+  @NotNull(message = "Le champ title est obligatoire")
+  @NotBlank(message = "Le champ title ne peut etre vide")
   private String title;
   private String description;
+
+  @NotNull(message = "Le champ isbn est obligatoire")
+  @NotBlank(message = "Le champ isbn ne peut etre vide")
   private String isbn;
   private int nombreDePage;
   private  String maisonEdition;
   private Date dateDeParution;
+
   private Date createdat;
   private Date updatedat;
 }
